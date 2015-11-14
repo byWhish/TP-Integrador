70d6fab983f51d221abfdf4f57eabc9723045d98
byWhish <lareugerardo@gmail.com>
2015-11-14 15:48:18 -0300

    Desde la Notebook

--- src/test/TestSistema.java
+++ src/test/TestSistema.java
@@ -1,6 +1,9 @@
 package test;
 
+import org.joda.time.DateTime;
+
 import junit.framework.TestCase;
+import sistema.Reserva;
 import sistema.Sistema;
 import usuario.Pasajero;
 import usuario.Usuario;
@@ -9,11 +12,13 @@ public class TestSistema extends TestCase{
 	
 	Sistema miSistema;
 	Usuario miUsuario;
+	Reserva miReserva;
 	
 	public void setUp(){
 		
 		miSistema = new Sistema();
 		miUsuario = new Pasajero( "tito@gmail.com","1234" );
+		miReserva = new Reserva(new DateTime(2015,12,1,0,0), new DateTime(2015,12,1,0,0), 2, new Pasajero( "tito@gmail.com", "1234"));
 		
 		miSistema.agregarUsuario( miUsuario );
 			
@@ -23,5 +28,9 @@ public class TestSistema extends TestCase{
 		//Aca testeo que me devuelva el usuario que se loguea
 		assertEquals( miUsuario, miSistema.logIn( "tito@gmail.com","1234" ));
 	}
-
+	
+	public void testFechaIngreso(){
+		//con esto me aseguro que la fecha sea futura
+		assert( miReserva.getFechaEntrada().isAfterNow());
+	}
 }
