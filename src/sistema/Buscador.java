0f39588517b02d6329c0c370e9241268cf5c7931
byWhish <lareugerardo@gmail.com>
2015-11-14 11:13:41 -0300

    Agrego Buscador

---
+++ src/sistema/Buscador.java
@@ -0,0 +1,14 @@
+package sistema;
+
+import org.joda.time.DateTime;
+
+public class Buscador {
+	
+	private DateTime fechaIngreso;
+	private DateTime fechaSalida;
+	private Integer cantidadPasajeros;
+	private String ciudad;
+	
+	
+
+}
